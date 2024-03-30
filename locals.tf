da118311e2ae1eea23c799a4edff25a7c89fcc92
Hameedullah Khan <hameed@sudoconsultants.com>
2024-03-31 00:51:02 +0400

    Init module

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  partition  = data.aws_partition.current.partition
+
+  # Add current user as key owner
+  key_owners = concat(data.aws_iam_session_context.current.issuer_arn, var.key_owners)
+
+
+  aliases = { for k, v in toset(var.aliases) : k => { name = v } }
+
+}
---
+++ main.tf
@@ -0,0 +1,214 @@
+# Alot of the code is copied from https://github.com/terraform-aws-modules/terraform-aws-kms
+# The code has been updated to provide security defaults.
+
+resource "aws_kms_key" "key" {
+  count = var.create ? 1 : 0
+
+  bypass_policy_lockout_safety_check = var.bypass_policy_lockout_safety_check
+  customer_master_key_spec           = var.customer_master_key_spec
+  custom_key_store_id                = var.custom_key_store_id
+  deletion_window_in_days            = var.deletion_window_in_days
+  description                        = var.description
+  enable_key_rotation                = var.enable_key_rotation
+  is_enabled                         = var.is_enabled
+  key_usage                          = var.key_usage
+  multi_region                       = var.multi_region
+  policy                             = coalesce(var.policy, data.aws_iam_policy_document.policy[0].json)
+
+  tags = var.tags
+}
+
+data "aws_iam_policy_document" "policy" {
+  count = var.create ? 1 : 0
+
+  source_policy_documents   = var.source_policy_documents
+  override_policy_documents = var.override_policy_documents
+
+  # This is default policy to allow the whole AWS account to perform all kms operations. Disabled by default.
+  dynamic "statement" {
+    for_each = var.enable_default_policy ? [1] : []
+
+    content {
+      sid       = "Default"
+      actions   = ["kms:*"]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = ["arn:${local.partition}:iam::${local.account_id}:root"]
+      }
+    }
+  }
+
+  # Key owner - all key operations
+  dynamic "statement" {
+    for_each = length(local.key_owners) > 0 ? [1] : []
+
+    content {
+      sid       = "KeyOwner"
+      actions   = ["kms:*"]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = local.key_owners
+      }
+    }
+  }
+
+  # Key administrators - https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-default-allow-administrators
+  dynamic "statement" {
+    for_each = length(var.key_administrators) > 0 ? [1] : []
+
+    content {
+      sid = "KeyAdministration"
+      actions = [
+        "kms:Create*",
+        "kms:Describe*",
+        "kms:Enable*",
+        "kms:List*",
+        "kms:Put*",
+        "kms:Update*",
+        "kms:Revoke*",
+        "kms:Disable*",
+        "kms:Get*",
+        "kms:Delete*",
+        "kms:TagResource",
+        "kms:UntagResource",
+        "kms:ScheduleKeyDeletion",
+        "kms:CancelKeyDeletion",
+        "kms:ReplicateKey",
+        "kms:ImportKeyMaterial"
+      ]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = var.key_administrators
+      }
+    }
+  }
+
+  # Key users - https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-default-allow-users
+  dynamic "statement" {
+    for_each = length(var.key_users) > 0 ? [1] : []
+
+    content {
+      sid = "KeyUsage"
+      actions = [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey",
+      ]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = var.key_users
+      }
+    }
+  }
+
+  # Key service users - https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-service-integration
+  dynamic "statement" {
+    for_each = length(var.key_service_users) > 0 ? [1] : []
+
+    content {
+      sid = "KeyServiceUsage"
+      actions = [
+        "kms:CreateGrant",
+        "kms:ListGrants",
+        "kms:RevokeGrant",
+      ]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = var.key_service_users
+      }
+
+      condition {
+        test     = "Bool"
+        variable = "kms:GrantIsForAWSResource"
+        values   = [true]
+      }
+    }
+  }
+
+  # Key service roles for autoscaling - https://docs.aws.amazon.com/autoscaling/ec2/userguide/key-policy-requirements-EBS-encryption.html#policy-example-cmk-access
+  dynamic "statement" {
+    for_each = length(var.key_service_roles_for_autoscaling) > 0 ? [1] : []
+
+    content {
+      sid = "KeyServiceRolesASG"
+      actions = [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey",
+      ]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = var.key_service_roles_for_autoscaling
+      }
+    }
+  }
+
+  dynamic "statement" {
+    for_each = length(var.key_service_roles_for_autoscaling) > 0 ? [1] : []
+
+    content {
+      sid = "KeyServiceRolesASGPersistentVol"
+      actions = [
+        "kms:CreateGrant"
+      ]
+      resources = ["*"]
+
+      principals {
+        type        = "AWS"
+        identifiers = var.key_service_roles_for_autoscaling
+      }
+
+      condition {
+        test     = "Bool"
+        variable = "kms:GrantIsForAWSResource"
+        values   = [true]
+      }
+    }
+  }
+}
+
+resource "aws_kms_alias" "alias" {
+  for_each = { for k, v in local.aliases : k => v if var.create }
+
+  name          = var.aliases_use_name_prefix ? null : "alias/${each.value.name}"
+  name_prefix   = var.aliases_use_name_prefix ? "alias/${each.value.name}-" : null
+  target_key_id = aws_kms_key.key[0].key_id
+}
+
+resource "aws_kms_grant" "grant" {
+  for_each = { for k, v in var.grants : k => v if var.create }
+
+  name              = try(each.value.name, each.key)
+  key_id            = aws_kms_key.key[0].key_id
+  grantee_principal = each.value.grantee_principal
+  operations        = each.value.operations
+
+  dynamic "constraints" {
+    for_each = length(lookup(each.value, "constraints", {})) == 0 ? [] : [each.value.constraints]
+
+    content {
+      encryption_context_equals = try(constraints.value.encryption_context_equals, null)
+      encryption_context_subset = try(constraints.value.encryption_context_subset, null)
+    }
+  }
+
+  retiring_principal    = try(each.value.retiring_principal, null)
+  grant_creation_tokens = try(each.value.grant_creation_tokens, null)
+  retire_on_delete      = try(each.value.retire_on_delete, null)
+}
