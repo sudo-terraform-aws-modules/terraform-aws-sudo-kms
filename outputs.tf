a225bb163df79b1aae0814e17d0e96e7c6026fc1
Hameedullah Khan <hameed@sudoconsultants.com>
2024-03-31 04:17:39 +0400

    fix: No output if key is not created

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "key_arn" {
   description = "The Amazon Resource Name (ARN) of the key"
-  value       = aws_kms_key.key[0].arn
+  value       = try(aws_kms_key.key[0].arn, null)
 }
 
 output "key_id" {
   description = "The globally unique identifier for the key"
-  value       = aws_kms_key.key[0].key_id
+  value       = try(aws_kms_key.key[0].key_id, null)
 }
